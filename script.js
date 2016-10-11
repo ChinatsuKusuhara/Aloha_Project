a11a1563af4049ff321dc22f7d8083e13da9b8fc
ermiljordan <ermiljordan64@gmail.com>
2016-10-11 02:51:19 -0700

    add second half of project

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+$(document).ready(function() {
+  $('selector').flickity().
+  $('.main-carousel').flickity({
+    groupCells: true,
+    cellAlign: 'left',
+    contain: true,
+    percentPosition:false
 
+  });
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
