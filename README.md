5a9d0646d499606a8c6b1401137be2f18134d0f5
ermiljordan <ermiljordan64@gmail.com>
2016-11-27 23:42:24 -0800

    REFACTORED

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
+## Aloha Apparel Co. project
 ##summary
-Aloha Apparel Co. is our first project that we re-created with HTML and CSS.
+This project is a recreation of an e-commerce website using 
+HTML, CSS and jQuery scripts. I have refactored the project using
+SCSS to make CSS styling cleaner. This site is built as a responsive
+website using mobile first approach.
+
 ##tools and languages
-In this project we used HTML and CSS.
+* HTML 
+* SCSS/CSS
+* JavaScript
+* jQuery for flickity
+
+## What I learnt
+In this project, where I refactored everything. I learned to be more
+patient and do a lot of test in order to get MOBILE FIRST approach.
+This project helped me improve on my SCSS/CSS skills. 
--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-$(document).ready(function() {
-  $('selector').flickity().
-  $('.main-carousel').flickity({
-    groupCells: true,
-    cellAlign: 'left',
-    contain: true,
-    percentPosition:false
-
-  });
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-
-  cellAlign: 'left',
-  contain: true
+$('.main-carousel').flickity({
+   cellAlign:'left',
+   contain:true,
+   percentPosition:false,
+   pageDots:true,
+   prevNextButtons:false,
+   autoPlay:1500,
+   imagesLoaded:true,
+   wrapAround:true,
 });
+// SMOOTH SCROLLING 
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -33,4 +23,37 @@ $(function() {
       }
     }
   });
+});
+// EMAIL 
+
+function validateEmail(sEmail) {
+    var filter = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    if (filter.test(sEmail)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+$(document).ready(function() {
+   $('.subButton').click(function() {
+        var sEmail = $('#email').val();
+        if ($.trim(sEmail).length == 0) {
+            alert('Please enter valid email address');
+            event.preventDefault();
+        }
+        
+        if (validateEmail(sEmail)) {
+            alert('Thanks for Registering! :)');
+            event.preventDefault();
+        }
+
+        else {
+            alert('Invalid Email Address');
+            event.preventDefault();
+        }
+
+    });
+
 });
